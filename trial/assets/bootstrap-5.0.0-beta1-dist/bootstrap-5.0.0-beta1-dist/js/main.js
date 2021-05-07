89a2498bce0ff19f2f1b2602973951c70441d3f3
Ridhwan <Ridhwan@DESKTOP-U6QCTJL>
2021-05-07 12:22:40 +0100

    Auto-generated commit

---
+++ trial/assets/bootstrap-5.0.0-beta1-dist/bootstrap-5.0.0-beta1-dist/js/main.js
@@ -0,0 +1,10 @@
+(function ($)
+  { "use strict"
+    $(window).on('load', function () {
+      $('#preloader-active').delay(1550).fadeOut('slow');
+      $('body').delay(450).css({
+        'overflow': 'visible'
+      });
+    });
+    
+})(jQuery);
